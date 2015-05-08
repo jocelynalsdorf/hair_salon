19f05de4d96db30019b9e10febb2c66ec93262f6
jocelynalsdorf <jfrodsla@gmail.com>
2015-05-08 07:49:19 -0700

    adds route handlers for adding a new stylist with post and listing them on stylists page

--- app.rb
+++ app.rb
@@ -10,3 +10,19 @@ DB = PG.connect({:dbname => "hair_salon"})
 get('/') do
   erb(:index)
 end
+
+get('/stylists/new') do
+  erb(:stylist_form)
+end
+
+get('/stylists') do
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+post('/stylists') do
+  name = params.fetch("name")
+  new_stylist = Stylist.new({:name => name, :id => nil})
+  new_stylist.save()
+  erb(:success)
+end
