f0fd4ab460f92338dd830604eb167246d7601012
jocelynalsdorf <jfrodsla@gmail.com>
2015-05-08 08:38:12 -0700

    add path for adding a client to a stylist; integration specs passing for route handler, web app working and holding data as expected

--- spec/1_integration_spec.rb
+++ spec/1_integration_spec.rb
@@ -26,4 +26,14 @@ describe('path to adding, deleting,viewing clients and stylists ', {:type => :fe
     expect(page).to have_content('Sally')
   end
 
+  it('allows user to click on stylist name and and view their clients') do
+    test_stylist = Stylist.new({:name => 'Judy', :id => nil})
+    test_stylist.save()
+    test_client = Client.new({:client_description => 'MrJones', :stylist_id => test_stylist.id()})
+    test_client.save()
+    visit('/stylists')
+    click_link(test_stylist.name())
+    expect(page).to have_content(test_client.client_description)
+  end
+
 end
