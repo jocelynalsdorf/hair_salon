3988418ef37239b5be4d0d634008e3daa3eae108
jocelynalsdorf <jfrodsla@gmail.com>
2015-05-08 07:42:40 -0700

    verifies server is loading index page, setting up basic route for adding a new stylist

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,16 +1,16 @@
-# require('capybara/rspec')
-# require('./app')
-# require('pry')
-# require('spec_helper')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
+require('capybara/rspec')
+require('./app')
+require('pry')
+require('spec_helper')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
 
-# describe('path to adding a new SOMETHING', {:type => :feature}) do
-#   it('allows user to add new SOMETHING from the home page') do
-#     visit('/')
-#     click_link('SOMETHING')
-#     fill_in('name', :with => 'Epicodus Work')
-#     click_button('Add List')
-#     expect(page).to have_content('SOMETHING!')
-#   end
-# end
+describe('path to adding, deleting,viewing clients and stylists ', {:type => :feature}) do
+  it('allows user to add new stylist from the home page') do
+    visit('/')
+    click_link('Add New Stylist')
+    fill_in('name', :with => 'Sally')
+    click_button('Add Stylist')
+    expect(page).to have_content('Sally')
+  end
+end
