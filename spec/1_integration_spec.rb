f557b1d2affb588cb406b0c498b48c44ab7d9631
jocelynalsdorf <jfrodsla@gmail.com>
2015-05-08 11:39:37 -0700

    adds integration testing for paths to the page where you can edit  and delete a stylist and their clients

--- spec/1_integration_spec.rb
+++ spec/1_integration_spec.rb
@@ -14,7 +14,7 @@ describe('path to adding, deleting,viewing clients and stylists ', {:type => :fe
     expect(page).to have_content('Success!')
   end
 
-  it('allows user to view  all stylists from the home page') do
+  it('allows user to view all stylists that have been added') do
     visit('/')
     click_link('Add New Stylist')
     fill_in('name', :with => 'Sally')
@@ -26,7 +26,7 @@ describe('path to adding, deleting,viewing clients and stylists ', {:type => :fe
     expect(page).to have_content('Sally')
   end
 
-  it('allows user to click on stylist name and and view their clients') do
+  it('allows user to click on stylist name to view/add their clients') do
     test_stylist = Stylist.new({:name => 'Judy', :id => nil})
     test_stylist.save()
     test_client = Client.new({:client_description => 'MrJones', :stylist_id => test_stylist.id()})
@@ -35,5 +35,16 @@ describe('path to adding, deleting,viewing clients and stylists ', {:type => :fe
     click_link(test_stylist.name())
     expect(page).to have_content(test_client.client_description)
   end
+
+  it('allows user to edit or delete a stylist') do
+    test_stylist = Stylist.new({:name => 'Judy', :id => nil})
+    test_stylist.save()
+    test_client = Client.new({:client_description => 'MrJones', :stylist_id => test_stylist.id()})
+    test_client.save()
+    visit("/stylists/#{test_stylist.id()}")
+    click_link('Edit this stylist')
+    expect(page).to have_content('Judy')
+  end
+
 
 end
